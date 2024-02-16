2b289b1d3059c10754de3226b68f823310b87090
imkhoat <khoatbui.idea@gmail.com>
2024-02-16 20:51:42 +0700

    feat: implement logic get persons from api

--- src/pages/challenge-two/_composable/usePerson.ts
+++ src/pages/challenge-two/_composable/usePerson.ts
@@ -1,53 +1,64 @@
 import { ref } from 'vue'
 import { Person } from 'src/types/person'
 import { createSharedComposable } from '@vueuse/core'
+import { HTTPS_STATUS } from 'src/constants/status-code'
+import { api } from 'src/boot/axios'
 
 
 function usePerson() {
   const persons = ref<Partial<Person>[]>([])
 
   async function fetchingPersons() {
-    persons.value = [
-      {
-        firstName: 'Miguel',
-        lastName: 'Bui',
-        email: 'miguelbui@gmail.com',
-        avatar: '/',
-        id: 1
+    // persons.value = [
+    //   {
+    //     firstName: 'Miguel',
+    //     lastName: 'Bui',
+    //     email: 'miguelbui@gmail.com',
+    //     avatar: '/',
+    //     id: 1
 
-      },
-      {
-        firstName: 'Miguel',
-        lastName: 'Bui',
-        email: 'miguelbui@gmail.com',
-        avatar: '/',
-        id: 2
+    //   },
+    //   {
+    //     firstName: 'Miguel',
+    //     lastName: 'Bui',
+    //     email: 'miguelbui@gmail.com',
+    //     avatar: '/',
+    //     id: 2
 
-      },
-      {
-        firstName: 'Miguel',
-        lastName: 'Bui',
-        email: 'miguelbui@gmail.com',
-        avatar: '/',
-        id: 3
+    //   },
+    //   {
+    //     firstName: 'Miguel',
+    //     lastName: 'Bui',
+    //     email: 'miguelbui@gmail.com',
+    //     avatar: '/',
+    //     id: 3
 
-      },
-      {
-        firstName: 'Miguel',
-        lastName: 'Bui',
-        email: 'miguelbui@gmail.com',
-        avatar: '/',
-        id: 4
+    //   },
+    //   {
+    //     firstName: 'Miguel',
+    //     lastName: 'Bui',
+    //     email: 'miguelbui@gmail.com',
+    //     avatar: '/',
+    //     id: 4
 
+    //   }
+    // ]
+
+    try {
+      const response = await api.get('https://fakerapi.it/api/v1/persons?_quantity=1&_gender=male&_birthday_start=2005-01-01')
+      if (response?.status === HTTPS_STATUS[200]) {
+        persons.value = response.data?.data||[]
       }
-    ]
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  function addPerson(){
+  function addPerson() {
 
   }
 
-  return { persons, fetchingPersons, addPerson}
+  return { persons, fetchingPersons, addPerson }
 }
 
 const useSharedPerson = createSharedComposable(usePerson)
